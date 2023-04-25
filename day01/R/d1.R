d5b99903393a82122f8ca8c61e0c2cb5aaacbcd2
harriscw <harriscw@ucla.edu>
2023-04-25 08:10:42 -0700

    R & gif

--- day01/R/d1.R
+++ day01/R/d1.R
@@ -8,6 +8,7 @@ acc=0
 seen=c()
 whilecnt=1
 start=Sys.time()
+thelength=length(input)
 while(TRUE){
   acc=acc+input[ind]
 
@@ -19,9 +20,33 @@ while(TRUE){
   }
   
   ind=ind+1
-  if(ind>length(input)){
+  if(ind>thelength){
     whilecnt=whilecnt+1
     if(whilecnt %% 10==0){print(whilecnt)}
     ind=1
+  }
+}
+
+###
+# Make some plots
+###
+
+library(lattice)
+
+mat=matrix(0,500,500) #initialize a 500x500 matrix of 0s
+cnt=1
+plotnum=1
+for(thisnum in seen){
+  row=floor(thisnum/500)+1 #every 500 start a new row
+  col=thisnum-500*floor(thisnum/500)+1 #take row out to get column
+  
+  mat[row,col]=1
+
+  cnt=cnt+1
+  if(cnt %% thelength==0){ #create a plot for each iteration of the input
+    png(filename=paste0("plot_",plotnum,".png"))
+    print(levelplot(mat))
+    dev.off()
+    plotnum=plotnum+1
   }
 }
